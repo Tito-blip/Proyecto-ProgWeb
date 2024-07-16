dc0479d317cea5cbba2c269cb2959b389e1b1685
Tito-blip <122133993+Tito-blip@users.noreply.github.com>
2024-07-16 19:22:35 -0400

    Delete paginaweb/app directory

--- paginaweb/app/extensions.py
+++
@@ -1,9 +0,0 @@
-# Inicializar en este archivo para evitar circularidad, luego inicializar en archivo de la app #
-
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-
-login_manager = LoginManager()
-login_manager.loging_view = 'auth.login'

fc41ee7ad4945fd54d1b2288838b702732549e43
Tito-blip <122133993+Tito-blip@users.noreply.github.com>
2024-07-16 19:48:08 -0400

    e

--- paginaweb/app/pagina.py
+++ paginaweb/app/pagina.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.secret_key = '97c27c7351b75853c654ce714a18b47f4a6399263b33a7d920ca2eb8d2d8c354'
 
 #Cambiar DATABASE_URI por PostgreSQL o Sqlite para utilizar una de las dos BD
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://user:password@localhost:5432' # Conexión con PostgreSQL
-#app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///local_db.sqlite' # Conexión con sqlite en reemplazo de postgres
+#app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://user:password@localhost:5432' # Conexión con PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///local_db.sqlite' # Conexión con sqlite en reemplazo de postgres
 
 app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'lux'
 app.config['FLASK_ADMIN_SWATCH'] = 'lux'
